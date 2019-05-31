c98ed21e7f83016128e1712785be01a69c0f602e
Ekaterina Podranyuk <e.podranyuk@yandex.ru>
2019-06-01 01:32:09 +0300

    added some coding settings

--- taxi_bigraph/main.cpp
+++ taxi_bigraph/main.cpp
@@ -1,9 +1,13 @@
 #include "taxi.h"
 #include <QtGui/QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	/* Настройки кодировки */
+	QTextCodec *tc=QTextCodec::codecForName("CP1251");
+	QTextCodec::setCodecForCStrings(tc);
 	Taxi w;
 	w.show();
 	return a.exec();
