321a689768d598e7f5b959f399cec5f0c7f514ad
Ekaterina Podranyuk <e.podranyuk@yandex.ru>
2019-06-01 15:33:40 +0300

    added code from qt example 'eastic nodes'

--- taxi_bigraph/main.cpp
+++ taxi_bigraph/main.cpp
@@ -1,6 +1,10 @@
 #include "taxi.h"
+#include "graphwidget.h"
+
 #include <QtGui/QApplication>
 #include <QTextCodec>
+#include <QTime>
+
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +12,12 @@ int main(int argc, char *argv[])
     /* Настройки кодировки */
     QTextCodec *tc=QTextCodec::codecForName("CP1251");
     QTextCodec::setCodecForCStrings(tc);
+
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+
+    GraphWidget widget;
+    widget.show();
+
     Taxi w;
     w.show();
     return a.exec();
