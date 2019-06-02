b6c8c13d0a5fddd7388ff82cc66101a5d84d36a8
AigulKh <aigul.kh017@gmail.com>
2019-06-02 22:53:26 +0300

    update

--- src/Edge.hpp
+++ src/Edge.hpp
@@ -1,4 +1,3 @@
-#include <random>
 #include "Vertex.hpp"
 
 template  <typename T, typename R>
@@ -6,15 +5,13 @@ class Edge {
     private:
         Vertex <T, R> x;
         Vertex <T, R> y;
-        int weight;
     public:
         Edge(Vertex <T, R> x, Vertex <T, R> y) {
             this->x = x;
             this->y = y;
-            this->weight = 1 + rand() % 100;
         }
 
-        ~Edge() { }
+        ~Edge() {}
 
         Vertex <T, R> getX() {
             return this->x;
@@ -24,12 +21,7 @@ class Edge {
             return this->y;
         }
 
-        int getWeight() {
-            return this->weight;
-        }
-
         bool operator==(Edge <T, R> e) {
             return ((this->x == e.getX() && this->y == e.getY()) || (this->x == e.getY() && this->y == e.getX()));
         }
-
 };
